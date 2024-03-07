005444cb9538acf81a82a8b1cf3b2b44061558ff
adwitajindal <adwita.jindal.42@gmail.com>
2024-03-07 22:17:14 +0530

    commit 1

--- C codes/sum_of_two_1_dimensional_matrix.c
+++ C codes/sum_of_two_1_dimensional_matrix.c
@@ -11,18 +11,28 @@ void main()
     // declaring arrays of the specified length
     int i;
     int a[length], b[length], c[length];
+
+    // input the value in first matrix
     for (i = 0; i < length; i++)
     {
         printf("Enter value [%d] (First matrix): ", i + 1);
         scanf("%d", &a[i]);
     }
+
+    // reinitialize value
     i = 0;
+
+    // input the value in second matrix
     for (i = 0; i < length; i++)
     {
         printf("Enter value [%d] (second matrix): ", i + 1);
         scanf("%d", &b[i]);
     }
+
+    // reinitialize value
     i = 0;
+
+    // getting the sum of the two matrix
     printf("Sum of the two matrix is [\t");
     for (i = 0; i < length; i++)
     {
