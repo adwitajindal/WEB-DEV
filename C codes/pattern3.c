7b2d3126f295b795b4c82bb3333651b7a2c5b75b
adwitajindal <adwita.jindal.42@gmail.com>
2024-03-13 11:09:15 +0530

    commit 1

---
+++ C codes/pattern3.c
@@ -0,0 +1,20 @@
+/*
+write a program (WAP) in C to print the following format
+4444
+333
+22
+1
+*/
+
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 4; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+            printf("%d  ", i);
+            printf("\n");
+    }
+    return 0;
+}
