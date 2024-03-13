4d10d5a1e839e77eb81db0f66746876d6e62a273
adwitajindal <adwita.jindal.42@gmail.com>
2024-03-14 01:43:08 +0530

    additional changes

--- C codes/diagonal_elements_of_matrix.c
+++ C codes/diagonal_elements_of_matrix.c
@@ -1,25 +1,45 @@
-// WAP in C to print the both diagonal elements in diagonal format of 2 D matrix order 3x3.
+// WAP in C to print the both diagonal elements in diagonal format of 2 D matrix order n*n.
 
 #include <stdio.h>
 int main()
 {
-    int row = 3;
-    int col = 3;
+    int row;
+    int col;
+    int i, j;
+    int max = 0;
+
+    printf("Input the no. of rows: ");
+    scanf("%d", &row);
+    printf("Input the no. of columns: ");
+    scanf("%d", &col);
+    if (col > row)
+    {
+        max += col;
+    }
+    else
+    {
+        max += row;
+    }
+
     int matrix[row][col];
     printf("Enter the elements of the matrix:\n");
-    for (int i = 0; i < row; i++)
+    for (i = 0; i < row; i++)
     {
-        for (int j = 0; j < col; j++)
+        for (j = 0; j < col; j++)
         {
             printf("Enter element at position (%d, %d): ", i + 1, j + 1);
             scanf("%d", &matrix[i][j]);
         }
     }
-    for (int i = 0; i < row; i++)
+    for (i = 0; i < row; i++)
     {
-        for (int j = 0; j < col; j++)
+        for (j = 0; j < col; j++)
         {
             if (i == j)
+            {
+                printf("  %d  ", matrix[i][j]);
+            }
+            else if ((i + j) == (max - 1))
             {
                 printf("  %d  ", matrix[i][j]);
             }
@@ -27,7 +47,6 @@ int main()
             {
                 printf("     ");
             }
-            
         }
         printf("\n");
     }
