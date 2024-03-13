10fb77237c1a80a6498a138db3935a2127544180
adwitajindal <adwita.jindal.42@gmail.com>
2024-03-14 00:14:02 +0530

    added a new file

--- C codes/transpose_matrix.c
+++ C codes/transpose_matrix.c
@@ -2,7 +2,38 @@
 #include <stdio.h>
 int main()
 {
-    int i,j;
-   
+    int row = 3;
+    int col = 3;
+    int matrix[row][col], transpose[row][col];
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("Enter element at position (%d, %d): ", i + 1, j + 1);
+            scanf("%d", &matrix[i][j]);
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+    printf("\nORIGINAL MATRIX\n");
+    for (int i = 0; i < col; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nTRANSPOSE MATRIX\n");
+    for (int i = 0; i < col; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            printf("%d\t", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
